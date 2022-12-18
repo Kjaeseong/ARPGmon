79dc152acc87d28a9065ab847294667031bc1b02
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-12-18 16:22:23 +0900

    [Feat]: SceneChanger 스크립트 추가

--- ARPGmonProject/Assets/02.Scripts/Util/GameManager.cs
+++ ARPGmonProject/Assets/02.Scripts/Util/GameManager.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class GameManager : SingletonBehaviour<GameManager>
 {
-    
+    public SceneChanger Scene { get; private set; }
+    public GpsSensor Gps { get; private set; }
+
+    private void Start() 
+    {
+        Scene = GetComponent<SceneChanger>();
+        Gps = GetComponentInChildren<GpsSensor>();
+    }
+
+
 }
