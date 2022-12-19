b9f1fc4f0f38f05fe704b6fb0e8b8915abd9f6b7
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-12-20 01:47:18 +0900

    [Feat]: MapRotateUI 추가

---
+++ ARPGmonProject/Assets/02.Scripts/UI/AdventureMainUI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdventureMainUI : MonoBehaviour
+{
+    [SerializeField] private MapRotateUI _mapRotateUI;
+    [SerializeField] private StatusUI _statusUI;
+    
+
+    public void RotateComplete()
+    {
+        //다음 UI 실행
+    }
+
+
+}
