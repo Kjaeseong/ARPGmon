462cc6523c6a90b1d443151b415cbb4c48255dfa
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-12-20 14:58:42 +0900

    [Feat]: 진화단계별 플레이어 몬스터 추가

---
+++ ARPGmonProject/Assets/07.Package/amusedART/StoneMonster/Script/Animation_Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation_Test : MonoBehaviour {
+
+	public const string IDLE	= "Anim_Idle";
+	public const string RUN		= "Anim_Run";
+	public const string ATTACK	= "Anim_Attack";
+
+	Animation anim;
+
+	void Start () 
+	{
+		anim = GetComponent<Animation>();
+	}
+	
+	public void IdleAni ()
+	{
+		anim.CrossFade (IDLE);
+	}
+
+	public void RunAni (){
+		anim.CrossFade (RUN);
+	}
+
+	public void AttackAni (){
+		anim.CrossFade (ATTACK);
+	}
+
+}
