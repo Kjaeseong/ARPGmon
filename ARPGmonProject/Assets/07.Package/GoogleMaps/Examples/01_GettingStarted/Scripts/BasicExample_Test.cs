9b43da2ef0952bd143fb5bcb0abe1da6783141e9
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-12-29 17:03:31 +0900

    [Feat]: PositionChecker 스크립트 추가

---
+++ ARPGmonProject/Assets/07.Package/GoogleMaps/Examples/01_GettingStarted/Scripts/BasicExample_Test.cs
@@ -0,0 +1,51 @@
+﻿using Google.Maps.Coord;
+using Google.Maps.Event;
+using Google.Maps.Examples.Shared;
+using UnityEngine;
+
+namespace Google.Maps.Examples {
+  /// <summary>
+  /// This example demonstrates a basic usage of the Maps SDK for Unity.
+  /// </summary>
+  /// <remarks>
+  /// By default, this script loads the Statue of Liberty. If a new lat/lng is set in the Unity
+  /// inspector before pressing start, that location will be loaded instead.
+  /// </remarks>
+  [RequireComponent(typeof(MapsService))]
+  public class BasicExample_Test : MonoBehaviour {
+    [Tooltip("LatLng to load (must be set before hitting play).")]
+    private LatLng _latLng = new LatLng(40.6892199, -74.044601);
+    private MapsService _mapsService;
+
+    /// <summary>
+    /// Use <see cref="MapsService"/> to load geometry.
+    /// </summary>
+    private void Start() 
+    {
+      // TODO : LatLng..사전에 입력해야 하는지는 추가적인 테스트 필요
+
+      _mapsService = GetComponent<MapsService>();
+      _mapsService.Events.MapEvents.Loaded.AddListener(OnLoaded);
+
+      LoadMap(37.539626, 127.123747);
+    }
+
+    public void LoadMap(double Lat, double Lng)
+    {
+      _latLng = new LatLng(Lat, Lng);
+      _mapsService.InitFloatingOrigin(_latLng);
+      _mapsService.LoadMap(ExampleDefaults.DefaultBounds, ExampleDefaults.DefaultGameObjectOptions);
+    }
+
+    /// <summary>
+    /// Example of OnLoaded event listener.
+    /// </summary>
+    /// <remarks>
+    /// The communication between the game and the MapsSDK is done through APIs and event listeners.
+    /// </remarks>
+    public void OnLoaded(MapLoadedArgs args) {
+      // The Map is loaded - you can start/resume gameplay from that point.
+      // The new geometry is added under the GameObject that has MapsService as a component.
+    }
+  }
+}
