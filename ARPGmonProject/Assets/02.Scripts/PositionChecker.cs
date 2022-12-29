91a836ad23197e1c225f9411fd5f6ee521bf4052
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-12-29 18:45:18 +0900

    [Feat]: 맵 위치보정 스크립트 추가

--- ARPGmonProject/Assets/02.Scripts/PositionChecker.cs
+++ ARPGmonProject/Assets/02.Scripts/PositionChecker.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class PositionChecker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _map;
+    [SerializeField] private float _rotateSpeed;
+    [SerializeField] private float _moveSpeed;
+
+    private Vector2 _movePosDiff;
+    private Vector2 _nowPos;
+    private Vector2 _prePos;
+
+    private void Start() 
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate() 
     {
-        
+        MapMove();
+    }
+
+    public void MapRotate(int Direction)
+    {
+        _map.transform.RotateAround(Camera.main.transform.position, Vector3.up, _rotateSpeed * Direction * Time.deltaTime);
+    }
+
+    private Vector2 PositionDrag()
+    {
+        _movePosDiff = Vector2.zero;
+        if(Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch (0);
+            if(touch.phase == TouchPhase.Began)
+            {
+                _prePos = touch.position - touch.deltaPosition;
+            }
+            else if(touch.phase == TouchPhase.Moved)
+            {
+                _nowPos = touch.position - touch.deltaPosition;
+                _movePosDiff = (Vector2)_prePos - _nowPos;
+                _prePos = touch.position - touch.deltaPosition;
+            }
+        }
+        return -_movePosDiff;
+    }
+
+    public void MapMove()
+    {
+        Vector2 move = PositionDrag();
+
+        _map.transform.position = new Vector3(
+            _map.transform.position.x + (move.x *_moveSpeed * Time.deltaTime),
+            _map.transform.position.y,
+            _map.transform.position.z + (move.y *_moveSpeed * Time.deltaTime)
+        );
+    }
+
+    public void PositionCheckEnd()
+    {
+        _map.transform.position = new Vector3(
+            _map.transform.position.x,
+            _map.transform.position.y - 1.3f,
+            _map.transform.position.z
+        );
+
+        gameObject.SetActive(false);
     }
 }
---
+++ ARPGmonProject/Assets/02.Scripts/UI/PositionCheckerUI.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PositionCheckerUI : MonoBehaviour
+{
+    [SerializeField] private Button _rotateLeft;
+    [SerializeField] private Button _rotateRight;
+
+    [SerializeField] private Button _okButton;
+
+    [SerializeField] private PositionChecker _checker;
+    private bool _isButtonDown;
+    private Coroutine _buttonDownCoroutine;
+    private int _direction;
+
+    private void Start() 
+    {
+        _okButton.onClick.AddListener(PositionCheckComplete);
+    }
+
+    private void FixedUpdate() 
+    {
+        _checker.MapMove();
+    }
+
+    public void ButtonDown(int Direction)
+    {
+        _isButtonDown = true;
+        _direction = Direction;
+        _buttonDownCoroutine = StartCoroutine(ButtonDownCoroutine());
+    }
+
+    public void ButtonUp()
+    {
+        _isButtonDown = false;
+        _direction = 0;
+        StopCoroutine(_buttonDownCoroutine);
+    }
+
+    private IEnumerator ButtonDownCoroutine()
+    {
+        while(true)
+        {
+            Debug.Log("Rotate");
+            _checker.MapRotate(_direction);
+            yield return GameManager.Instance.Cycle;
+        }
+    }
+
+    private void PositionCheckComplete()
+    {
+        _checker.PositionCheckEnd();
+        gameObject.SetActive(false);
+    }
+}
